ff12d2dffb00d15c5b79b3a65f0249d5c1730837
WhiteRhin0 <white.rh42@gmail.com>
2019-09-25 14:45:35 +0300

    Add test

--- binary.rb
+++ binary.rb
@@ -1,7 +1,13 @@
+require 'minitest/autorun'
+
+
 def binary_shift(value, n, m)
   value = ((1 << n) | value)
   return ((1 << m) | value)
 end
 
-
-p binary_shift(10,0 , 2)
+class Tests < Minitest::Test
+  def test_ok
+    assert_equal 31, binary_shift(21, 1, 3)
+  end
+end
